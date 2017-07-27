403a0503ad3d87d7d2c728981372c9d53265318d
Tom White <thomas.white5@mohawkcollege.ca>
2017-07-27 15:14:40 -0400

    Setup routing, home page, bootstrap 4

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRouterModule { }
