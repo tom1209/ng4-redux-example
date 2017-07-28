e5d2956df8ab3d042ea7c7d88a27eedaedf4d89d
Tom White <thomas.white5@mohawkcollege.ca>
2017-07-28 09:14:41 -0400

    Changed view runs to a table

--- src/app/services/runTracker.service.ts
+++ src/app/services/runTracker.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 const baseUrl = 'https://restfulruntracker.herokuapp.com/api';
